6d12aa984ff7b2f306d8953a039c58042c323fda
Alexei Okhapkin <alexeyyago@gmail.com>
2024-02-23 17:45:39 +0300

    fix eslint

---
+++ movie-app/src/components/App/App.jsx
@@ -0,0 +1,12 @@
+import Layout, {Content, Header} from 'antd/es/layout/layout';
+
+function App() {
+  return (
+    <Layout>
+      <Header>Head</Header>
+      <Content>Main</Content>
+    </Layout>
+  );
+}
+
+export {App};
