6cbb8b4405c2b8f243f5bdf9f34215fd77cc4f5f
Alexei Okhapkin <alexeyyago@gmail.com>
2024-03-05 14:01:28 +0300

    MovieCard

--- movie-app/src/components/MovieCard/MovieCard.jsx
+++ movie-app/src/components/MovieCard/MovieCard.jsx
@@ -1,9 +1,14 @@
 import { Component } from 'react';
+import { parseISO, format } from 'date-fns';
+import { Typography } from 'antd';
+
+import NoImage from './test_img.jpg';
 
 export default class MovieCard extends Component {
   constructor(props) {
     super(props);
     this.maxOverviewLength = 240;
+    this.posterUrl = 'https://image.tmdb.org/t/p/w500';
   }
 
   trimOverview(text) {
@@ -16,7 +21,29 @@ export default class MovieCard extends Component {
   }
 
   render() {
+    // eslint-disable-next-line react/prop-types
     const {title, date, overview, poster} = this.props;
-    
+    const { Title } = Typography;
+    let formattedMovieDate = '';
+    if (date) {
+      formattedMovieDate = format(parseISO(date), 'MMMM d, y');
+    }
+    const trimmedOverview = this.trimOverview(overview);
+    return (
+      <div className="card">
+        <div className="card__image">
+          <img src={poster ? this.posterUrl + poster : NoImage} />
+        </div>
+        <div className="card_description">
+          <Title className='card__title' level={3}>{title}</Title>
+          <p className='card__date'>{formattedMovieDate}</p>
+          <ul className="genres-list">
+            <li className="genres-list__item">Action</li>
+            <li className="genres-list__item">Drama</li>
+          </ul>
+          <p className='card__overview'>{trimmedOverview}</p>
+        </div>
+      </div>
+    )
   }
 }
